b79904e7f48b33fc379e1893dd05fd69d9df6dca
emilyalv <emilyaalv@gmail.com>
2021-04-27 10:11:08 -0500

    new files for db updates

---
+++ tests/Employee.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('@jest/globals')
+const Employee = require('../lib/Employee')
+
+test ('creation of employee object', () => {
+    const employee = new Employee ('Emily', '123', 'emily@gmail.com')
+    expect (typeof(employee)).toBe('object')
+})
+
+test ('getName returns employee name', () =>{
+    const employee = new Employee ('Emily', '123', 'emily@gmail.com')
+    expect (employee.getName()).toBe('Emily')
+})
